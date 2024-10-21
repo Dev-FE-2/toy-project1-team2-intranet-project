1cd20316fb09ac504f37d090367893a696f98a17
bbjbc <jobc4550@gmail.com>
2024-10-21 22:25:03 +0900

    refactor: 전체적인 레이아웃 구조 리팩토링

--- src/components/layouts/Layout.js
+++ src/components/layouts/Layout.js
@@ -1,31 +1,25 @@
 import Header from '../headers/Header';
-import Navbar from '../navbars/Navbar';
+import Navbar from '../nav-bars/Navbar';
 
 export default class Layout {
-  constructor(getUserRole) {
+  constructor() {
     this.header = new Header();
     this.navBar = new Navbar();
-    this.getUser = getUserRole;
-    this.render(); // 헤더와 네비게이션 바를 렌더링
+    this.contentContainer = document.getElementById('content');
   }
 
   render() {
-    const headerElement = document.getElementsByClassName('.header');
-    const navbarElement = document.getElementsByClassName('.navbar');
-    const contentContainer = document.getElementById('content');
-
-    headerElement.innerHTML = ''; // 기존 헤더 내용 초기화
-    navbarElement.innerHTML = ''; // 기존 네비게이션 바 내용 초기화
-    contentContainer.innerHTML = ''; // 기존 내용 초기화
-
     this.header.render(); // 헤더 렌더링
     this.navBar.render(); // 네비게이션 바 렌더링
+    this.updateActiveNavLink(window.location.pathname);
   }
 
   setContent(component) {
-    const contentContainer = document.getElementById('content');
-    contentContainer.innerHTML = ''; // 기존 내용을 초기화
-    component.render(); // 새로운 내용을 렌더링
-    contentContainer.appendChild(component.getElement()); // 컴포넌트를 추가
+    this.contentContainer.innerHTML = '';
+    component.render(this.contentContainer);
+  }
+
+  updateActiveNavLink(path) {
+    this.navBar.updateActiveLink(path);
   }
 }
